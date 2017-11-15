c1b20965a2d5518af221d3b057e85ccc68ec65d5
Dorian <devdnhee>
2017-11-15 14:51:16 +0100

    models and transform notebook, vanilla svm

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,11 @@
 import csv
+import pandas as pd
 
 variables=['PassengerId','Survived','Pcass','Name','Sex','Age','SibSp','Parch','Ticket',
            'Fare','Cabin','Embarked']
-titles=['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Don.', 'Rev.', 'Dr.', 'Mme.',
-         'Ms.', 'Major.', 'Lady.', 'Sir.', 'Mlle.', 'Col.', 'Capt.',
-         'Jonkheer.']
-
-embarkments=['S', 'C', 'Q']
+TITLES_ARRAY = ['Mr', 'Mrs', 'Miss', 'Master', 'Don', 'Rev', 'Dr', 'Mme', 'Ms', 'Major', 'Lady', 'Sir', 'Mlle', 'Col',
+                'Capt', 'the Countess', 'Jonkheer']
+EMBARKMENT_ARRAY = ['S', 'C', 'Q']
 
 def get_titles():
     '''Mr., Miss., ...'''
@@ -18,7 +17,7 @@ def get_titles():
             title=first.split()[0]
             if title not in title_l:
                 title_l.append(title)
-        print title_l
+        print(title_l)
 
 def get_labels(var):
     '''Mr., Miss., ...'''
@@ -28,7 +27,7 @@ def get_labels(var):
         for row in reader:
             if row[var] not in var_l:
                 var_l.append(row[var])
-        print var_l 
+        print(var_l)
         
 def read_train():
     '''
@@ -38,7 +37,7 @@ def read_train():
     with open('train.csv','r') as f:
         reader=csv.DictReader(f)
         for row in reader:
-            print row,'\n',dict_to_feat(row)
+            print(row,'\n',dict_to_feat(row))
             data.append(row) 
 
 def dict_to_feat(sample):
@@ -56,16 +55,18 @@ def dict_to_feat(sample):
     return passenger_id,[survived,pclass,sex,age,sibsp,parch,fare]+name
 
 def embarked_features(embarked):
-    emb_f=len(embarkments)*[0.]
+    emb_f=len(EMBARKMENT_ARRAY)*[0.]
     try:
-        emb_f[embarkments.index(embarked)]=1.0
+        emb_f[EMBARKMENT_ARRAY.index(embarked)]=1.0
+    except:
+        raise
 
 def name_features(name):
     length=float(len(name))
     title=(name.split(',')[1]).split()[0]
-    title_f=len(titles)*[0.]
+    title_f=len(TITLES_ARRAY)*[0.]
     try:
-        title_f[titles.index(title)]=1. 
+        title_f[TITLES_ARRAY.index(title)]=1.
     except:
         pass
     n_names=float(len(name.split()))
@@ -77,8 +78,44 @@ def sex_feature(sex):
 def age_feature(age):
     return float(age) if age!='' else None
 
+def transform_df(df):
+    """
+    transform the raw titanic dataset to be processable
+    :param df: dataframe containing the titanic data
+    :return: processed dataframe
+    """
+    df['length'] = df['Name'].apply(len)
+    df['Title'] = df['Name'].str.extract(', ?([^\.]*)\.', expand=True)
+    onehot_title_df = pd.get_dummies(df['Title'], columns=TITLES_ARRAY)
+    df = pd.concat([df, onehot_title_df], axis=1, join='inner')
+    df['Sex'] = df['Sex'].apply(lambda x: 1. if x == 'male' else -1.)
+    onehot_embarked_df = pd.get_dummies(df['Embarked'], columns=EMBARKMENT_ARRAY)
+    df = pd.concat([df, onehot_embarked_df], axis=1, join='inner')
+    df = df.drop(['Name', 'Ticket', 'Cabin', 'Embarked', 'Title'], axis=1)
+    for column in ['Age', 'Fare']:
+        avg_value = df[column].mean()
+        df[column] = df['Age'].fillna(avg_value)
+
+    # fix missing columns
+    c1 = set(EMBARKMENT_ARRAY) | set(TITLES_ARRAY)
+    c2 = set(list(onehot_embarked_df)) | set(list(onehot_title_df))
+    missing_cols = c1 - c2
+    for c in missing_cols:
+        df[c] = 0
+
+    # drop additional columns
+    add_cols = c2 - (c1 & c2)
+    df = df.drop(add_cols, axis=1)
+
+    return df
+
+def transform_csv(input_filepath, output_filepath):
+    df = pd.read_csv(input_filepath)
+    df = transform_df(df)
+    df.to_csv(path_or_buf=output_filepath, index=False)
+
 if __name__=='__main__':
-    read_train()
+    #read_train()
     #get_titles()
     #get_labels('Ticket')
-    get_labels('Embarked')
+    transform_csv('test.csv', 'test_processed.csv')
